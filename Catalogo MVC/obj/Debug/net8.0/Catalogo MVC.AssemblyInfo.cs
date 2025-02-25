7a721e87c6c2aa1e0c5f0d367ad6d7c903f68b2c
walter potma <145157901+waltindobne@users.noreply.github.com>
2025-02-25 17:59:18 -0300

    .

--- Catalogo MVC/obj/Debug/net8.0/Catalogo MVC.AssemblyInfo.cs
+++ Catalogo MVC/obj/Debug/net8.0/Catalogo MVC.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalogo MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a55acf2bc2ba902cc05fbcab27c33edc94e8f577")]
 [assembly: System.Reflection.AssemblyProductAttribute("Catalogo MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Catalogo MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
